9de1553158770c1361f85ba5b4355cf2f4fff718
Avantika Shende <avantika.shende@apptware.com>
2025-12-15 12:35:11 +0530

    Checking access

--- src/app/product-overview/page.tsx
+++ src/app/product-overview/page.tsx
@@ -2,7 +2,7 @@
 export default function ProductOverview() {
   return (
    <div className="page-wrapper">
-      <h1 className="page-title-centered">Product Overview</h1>
+      <h1 className="page-title-centered">Product Overview Page</h1>
     </div>
   );
 }
